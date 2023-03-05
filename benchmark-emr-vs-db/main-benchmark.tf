d01832a7fad3e60c5de4fcdd96dbb7f8495fcd0a
Philip J. Reed <reed9999@users.noreply.github.com>
2023-03-04 23:15:59 -0500

    add several files - now broken
    I thought this had been checkpointed sooner. Why is the Aurora stuff now broken?

--- benchmark-emr-vs-db/main-benchmark.tf
+++ benchmark-emr-vs-db/main-benchmark.tf
@@ -10,15 +10,10 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-2"
+  # region  = "us-east-2"
+  # Just for fun... Sa~o Paulo
+  region  = "sa-east-1"
 }
-
-# This is only a prototype-quality way of handling secrets
-# It depends on env vars:
-# ```
-#  export TF_VAR_rds_username=...
-#  export TF_VAR_rds_password=...
-# Plan to read up on better ways to handle secrets.
 
 variable "rds_username" {
   description = "The username for the RDS DB main user"
@@ -35,7 +30,7 @@ resource "aws_rds_cluster" "rds" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.10.2"
-  availability_zones      = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  # availability_zones      = ["us-east-2a", "us-east-2b", "us-east-2c"]
   database_name           = "mydb"
   master_username         = var.rds_username
   master_password         = var.rds_password
@@ -44,38 +39,16 @@ resource "aws_rds_cluster" "rds" {
   preferred_backup_window = "07:00-09:00"
 }
 
+
 resource "aws_rds_cluster_instance" "cluster_instances" {
-  count              = 2
-  identifier         = "aurora-cluster-demo-${count.index}"
-  cluster_identifier = aws_rds_cluster.rds.id
-  # instance_class     = "db.r4.large"
-  instance_class     = "db.t2.small"
-  engine             = aws_rds_cluster.rds.engine
-  engine_version     = aws_rds_cluster.rds.engine_version
-  publicly_accessible = true 
-}
+    count              = 2
+    identifier         = "aurora-cluster-demo-${count.index}"
+    cluster_identifier = aws_rds_cluster.rds.id
+    # instance_class     = "db.r4.large"
+    instance_class     = "db.t2.small"
+    engine             = aws_rds_cluster.rds.engine
+    engine_version     = aws_rds_cluster.rds.engine_version
+    publicly_accessible = true 
+  }
 
-# It seems the aws_db_instance below was never the right way to do this!
-
-# See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance
-# Note that a cluster without an instance will have endpoints that sit in "Creating" status forever,
-# never accessible from a MYSQL client.
-# resource "aws_db_instance" "db_instance" {
-#     # Rather opaquely, omitting this defaults to "aurora" which is apparently a dummy setting.
-#   # https://stackoverflow.com/a/70930027
-#   # storage_type = "gp2"
-
-#   allocated_storage    = 50
-#   max_allocated_storage    = 200
-#   engine               = "aurora-mysql"
-#   engine_version       = "5.7.mysql_aurora.2.10.2"
-#   # See https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/Concepts.DBInstanceClass.html 
-#   instance_class       = "db.t2.small"
-#   db_name                 = "mydb"
-#   username             = var.rds_username
-#   password             = var.rds_password
-#   parameter_group_name = "default.mysql5.7"
-#   skip_final_snapshot  = true
-
-# }
 
--- benchmark-emr-vs-db/main.sh
+++ benchmark-emr-vs-db/main.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/sh
 # Quick start script to run our main benchmark sequence -- which is in evolution.
 
+export MYSQL_CLIENT="/c/Program Files/MySQL/MySQL Workbench 8.0/mysql" 
+export OTHER_MYSQL_CLIENT="/c/Program Files/MySQL/MySQL Shell 8.0/bin/mysqlsh.exe" 
+
 function do_body {
-    ls -lrt 
+    echo
+    echo "********************************************************************************"
+    echo "This is your chance to do something interesting" 
+    echo "You have 2 hours until I will shut down. (Or you can kill the script and risk" 
+    echo "forgetting to \`terraform destroy\`)."
+    echo "********************************************************************************"
+    date 
+    echo
+    terraform show | grep endpoint
+    # This isn't quite right -- need to `cut` it as well
+    export AURORA_ENDPOINT=`terraform show | grep endpoint`
+    # Try this when I get the chance: https://stackoverflow.com/questions/46536360/
+    $MYSQL_CLIENT --help
+    sleep 2h
+    date 
+    echo "Time's up! Now it's time to tear down."
 }
 
 # Informational
 terraform validate
 terraform plan 
 
-# For the moment, Terraform still needs to prompt for database username and password; 
 # I will ameliorate this soon. 
 terraform apply -auto-approve
 do_body
